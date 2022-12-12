808f1a9c1672f11daf8928f628e37ce16b3fa159
talal <talaldev@protonmail.com>
2022-12-12 11:02:07 +0200

    D01P02

--- day01/calories.py
+++ day01/calories.py
@@ -2,4 +2,8 @@ with open('input.txt', 'r') as f:
 	# Part 01
 	calories = [sum([int(i) for i in x.split('\n') if i]) for x in f.read().split('\n\n')]
 	max_cal = max(calories)
-	print(max_cal)
+	# print(max_cal)
+
+	# Part 02
+	calories.sort()
+	print(sum(calories[-3:]))
