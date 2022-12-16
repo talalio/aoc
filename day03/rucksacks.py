a831cfc9f7a618c45041395b0ff648fc296b6b08
talal <talaldev@protonmail.com>
2022-12-16 21:48:44 +0200

    D03P02

--- day03/rucksacks.py
+++ day03/rucksacks.py
@@ -1,9 +1,11 @@
 from string import ascii_letters
 
 with open('input.txt', 'r') as f:
-    items = [[x[:len(x)//2], x[len(x)//2:]] for x in f.read().split('\n') if x]
-    prio_sum = sum([(ascii_letters.find(c) + 1) 
-        for c in ascii_letters 
-        for item in items 
-        if (c in item[0]) and (c in item[1])])
-    print(prio_sum)
+    items = [x for x in f.read().split('\n') if x]
+    groups = [items[i:i+3] for i in range(0, len(items), 3)]
+    badges_prio = [(ascii_letters.find(c) + 1)
+            for group in groups 
+            for c in set(group[0])
+            if (c in group[1]) and (c in group[2])]
+    badges_prio_sum = sum(badges_prio)
+    print(badges_prio_sum)
