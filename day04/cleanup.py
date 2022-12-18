fd8958dcc5353c0f077b999f8052189255cb72d0
talal <talaldev@protonmail.com>
2022-12-18 22:12:50 +0200

    D04P02

--- day04/cleanup.py
+++ day04/cleanup.py
@@ -4,10 +4,10 @@ with open('input.txt', 'r') as f:
     overlap = 0
 
     for pair in pairs:
-        r1 = range(int(pair[0].split('-')[0]), int(pair[0].split('-')[1]))
-        r2 = range(int(pair[1].split('-')[0]), int(pair[1].split('-')[1]))
+        r1 = set(range(int(pair[0].split('-')[0]), int(pair[0].split('-')[1]) + 1))
+        r2 = set(range(int(pair[1].split('-')[0]), int(pair[1].split('-')[1]) + 1))
 
-        if ((r1.start >= r2.start) and (r1.stop <= r2.stop)) or ((r2.start >= r1.start) and (r2.stop <= r1.stop)):
+        if r1.intersection(r2) != set():
             overlap += 1
 
     print(overlap)
